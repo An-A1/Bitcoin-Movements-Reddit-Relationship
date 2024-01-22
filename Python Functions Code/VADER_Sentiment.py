daaa5d5efe9f95b89beadf3f82b49a48c6a644ab
bolishakim <149377305+bolishakim@users.noreply.github.com>
2024-01-22 18:42:19 +0100

    Update VADER_Sentiment.py

--- Python Functions Code/VADER_Sentiment.py
+++ Python Functions Code/VADER_Sentiment.py
@@ -2,8 +2,8 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 from tqdm import tqdm
 
-def perform_sentiment_analysis(file_path):
-    data = pd.read_csv(file_path)
+def VADER_sentiment_analysis(input_csv, output_csv):
+    data = pd.read_csv(input_csv)
     data = data.dropna(subset=['comment_body'])
     data['comment_body'] = data['comment_body'].astype(str)
     vader = SentimentIntensityAnalyzer()
@@ -16,7 +16,7 @@ def perform_sentiment_analysis(file_path):
         data["VADER_compound"][i] = float(sentiment_dict['compound'])
 
     data["VADER_class"] = ""
-    for i in range(len(data['VADER_compound'])):
+    for i in tqdm(range(len(data['VADER_compound'])), desc="Assigning Classes"):
         compound_score = data['VADER_compound'][i]
         if compound_score > 0.5:
             data["VADER_class"][i] = "Positive"
@@ -36,7 +36,7 @@ def perform_sentiment_analysis(file_path):
     print(mean_sentiments)
 
     data = data[data['VADER_compound'] != 0]
-    data.to_csv("sentiment_all_subreddits_data.csv", index=False)
+    data.to_csv(output_csv, index=False)
 
-
-#perform_sentiment_analysis('sentiment_all_subreddits_data.csv')
+# Example usage:
+# VADER_sentiment_analysis(input_csv='Reddit_new.csv', output_csv='VADER_sentiment_data.csv')
